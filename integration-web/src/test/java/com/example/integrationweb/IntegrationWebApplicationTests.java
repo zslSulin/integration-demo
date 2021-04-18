aa176ebe76acb1641a4ca37986f3ddad1c0bbf60
chisu <chisu@myweimai.com>
2021-04-18 09:16:28 +0800

    init

---
+++ integration-web/src/test/java/com/example/integrationweb/IntegrationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integrationweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
